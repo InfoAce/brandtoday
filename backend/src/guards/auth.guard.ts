13833a2bdfa0ed50a05090a92ac5c238f377215a
Bryant Rotich <bryantkrotich@gmail.com>
2024-04-02 00:02:01 +0300

    Fix: auth guard

--- backend/src/guards/auth.guard.ts
+++ backend/src/guards/auth.guard.ts
@@ -1,39 +1,35 @@
 import {
     CanActivate,
     ExecutionContext,
-    Inject,
     Injectable,
     UnauthorizedException,
-  } from '@nestjs/common';
-  import { JwtService } from '@nestjs/jwt';
-  import { Request } from 'express';
-  import { DataSource } from 'typeorm';
-  import { UserEntity } from '../entities';
-  import { pick, set } from 'lodash';
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { set } from 'lodash';
+import { ConfigService } from '@nestjs/config';
+import { UserModel } from 'src/models';
   
   @Injectable()
   export class AuthGuard implements CanActivate {
-  
-    private readonly userEntity;
-    
+
     constructor(
       private jwtService: JwtService,
-      @Inject(DataSource) private readonly dataSource: DataSource    
-    ) {
-      this.userEntity = this.dataSource.getRepository(UserEntity);
-    }
+      private configService: ConfigService,
+      private userModel: UserModel 
+    ) {}
   
     async canActivate(context: ExecutionContext): Promise<boolean> {
       const request = context.switchToHttp().getRequest();
-      const token = this.extractTokenFromHeader(request);
+      const token   = this.extractTokenFromHeader(request);
 
       if (token == undefined) {
         throw new UnauthorizedException();
       }
 
       try {
-        const { user: { email } } = await this.jwtService.verifyAsync(token,{secret: process.env.JWT_SESSION_KEY});
-        const authUser            =  await this.userEntity.findOneBy({email});
+        const { user: { email } } = await this.jwtService.verifyAsync(token,{secret: this.configService.get<string>('app.JWT_SESSION_KEY') });
+        const authUser            =  await this.userModel.findOneBy({email});
         // 💡 We're assigning the payload to the request object here
         // so that we can access it in our route handlers
         set(request,'user',authUser);
