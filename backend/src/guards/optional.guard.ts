e3fbfd45c3469af5686456c8adc5de247456c0a0
Bryant Rotich <bryantkrotich@gmail.com>
2024-04-02 00:06:34 +0300

    Fix: optional guard

--- backend/src/guards/optional.guard.ts
+++ backend/src/guards/optional.guard.ts
@@ -1,50 +1,46 @@
 import {
-    CanActivate,
-    ExecutionContext,
-    Inject,
-    Injectable,
-    UnauthorizedException,
-  } from '@nestjs/common';
-  import { JwtService } from '@nestjs/jwt';
-  import { Request } from 'express';
-  import { DataSource } from 'typeorm';
-  import { UserEntity } from '../entities';
-  import { pick, set } from 'lodash';
-  
-  @Injectable()
-  export class OptionalGuard implements CanActivate {
-  
-    private readonly userEntity;
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { set } from 'lodash';
+import { ConfigService } from '@nestjs/config';
+import { UserModel } from 'src/models';
+
+@Injectable()
+export class OptionalGuard implements CanActivate {
     
-    constructor(
-      private jwtService: JwtService,
-      @Inject(DataSource) private readonly dataSource: DataSource    
-    ) {
-      this.userEntity = this.dataSource.getRepository(UserEntity);
-    }
-  
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-      const request = context.switchToHttp().getRequest();
-      const token = this.extractTokenFromHeader(request);
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private userModel: UserModel 
+  ) {}
 
-      if (token != undefined) {
-        try {
-            const { user: { email } } = await this.jwtService.verifyAsync(token,{secret: process.env.JWT_SESSION_KEY});
-            const authUser            = await this.userEntity.findOneBy({email});
-            // 💡 We're assigning the payload to the request object here
-            // so that we can access it in our route handlers
-            set(request,'user',authUser);
-            // request.user =;
-          } catch(err) {
-            throw new UnauthorizedException();
-          }
-      }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
 
-      return true;
+    if (token != undefined) {
+      try {
+        const { user: { email } } = await this.jwtService.verifyAsync(token,{secret: this.configService.get<string>('app.JWT_SESSION_KEY') });
+        const authUser            = await this.userModel.findOneBy({email});
+          // 💡 We're assigning the payload to the request object here
+          // so that we can access it in our route handlers
+          set(request,'user',authUser);
+          // request.user =;
+        } catch(err) {
+          throw new UnauthorizedException();
+        }
     }
-  
-    private extractTokenFromHeader(request: Request): string | undefined {
-      const [type, token] = request.headers.authorization?.split(' ') ?? [];
-      return type === 'Bearer' ? token : undefined;
-    }
+
+    return true;
   }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
