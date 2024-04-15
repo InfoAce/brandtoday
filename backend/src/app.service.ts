22463747d34b316a165997a03b37ed8f89536a81
Bryant Rotich <bryantkrotich@gmail.com>
2024-04-15 09:36:34 +0300

    Update: cron jobs

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -17,49 +17,63 @@ export class AppService {
   async amrodCredentials(){
     
     try {
+    
       this.amrodService.login();
+
     } catch (err) {
+    
       console.log(err);
+    
     }
 
   }
 
-  @Cron(CronExpression.EVERY_30_MINUTES)
-  async updateAmrodData(){
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  async updateProductsPricesBrands(){
 
     try {
-
       // Fetch amrod product categories
       let categories = await this.amrodService.getCategories();  
 
-      // Fetch amrod products
-      let products = await this.amrodService.getProducts();    
-      
       // Fetch amrod prices
       let prices = await this.amrodService.getPrices();   
-      
-      // Fetch amrod prices
-      let stock = await this.amrodService.getStock();   
 
       // Fetch amrod brands
-      let brands = await this.amrodService.getBrands();         
+      let brands = await this.amrodService.getBrands();  
+    
+      // Fetch amrod products
+      let products = await this.amrodService.getProducts();    
 
       await this.cacheManager.store.set('amrod_categories',categories);
 
       await this.cacheManager.store.set('amrod_products',products);
 
       await this.cacheManager.store.set('amrod_prices',prices);
-    
-      await this.cacheManager.store.set('amrod_stock',stock);
-
+      
       await this.cacheManager.store.set('amrod_brands',brands);
       
     } catch (err) {
 
-      console.log(err);
-    
     }
-
   }
 
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async updateStock(){
+
+    try {
+
+      // Fetch amrod prices
+      let stock = await this.amrodService.getStock();  
+
+      // Fetch amrod brands
+      let brands = await this.amrodService.getBrands();  
+    
+      await this.cacheManager.store.set('amrod_stock',stock);
+
+      await this.cacheManager.store.set('amrod_brands',brands);
+
+    } catch (err) {
+
+    }
+  }
 }
