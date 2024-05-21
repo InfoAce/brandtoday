2ebf168ee822408aea419f4c036bb9fe2265cf47
Bryant Rotich <bryantkrotich@gmail.com>
2024-05-21 13:27:28 +0300

    Fix: synchronize amrod

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -19,7 +19,7 @@ export class AppService {
     this.config = this.configService.get<string>('services.amrod');  
   }
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(CronExpression.EVERY_DAY_AT_11PM)
   async updateProductsPricesBrands(){
 
     try {
