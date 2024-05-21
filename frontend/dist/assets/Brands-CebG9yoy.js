d0e94cb231099ff8128b4bd5e986c6a7c2ce48b0
Bryant Rotich <bryantkrotich@gmail.com>
2024-05-21 13:19:58 +0300

    Update: build frontend

---
+++ frontend/dist/assets/Brands-CebG9yoy.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-BX7DgPRM.js";const n={};function c(e,t){return null}const s=r(n,[["render",c]]);export{s as default};
---
+++ frontend/dist/assets/Overview-C2p_f0s3.js
@@ -0,0 +1,1 @@
+import{_ as e,e as r,f as t}from"./index-BX7DgPRM.js";const o={name:"Overview"};function c(n,s,a,_,p,f){return r(),t("div")}const d=e(o,[["render",c]]);export{d as default};
