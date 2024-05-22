c38f4f879f145b4669372b745d74c8be0cf4299b
Bryant Rotich <bryantkrotich@gmail.com>
2024-05-22 14:15:17 +0300

    Update: amrod data sets

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,13 +3,16 @@ import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { AmrodService, MailService } from './services';
 import { ConfigService } from '@nestjs/config';
+import { sep } from 'path';
 
 @Injectable()
 export class AppService {
 
   private config;
 
-  private readonly logger = new Logger(AppService.name);
+  private readonly logger    = new Logger(AppService.name);
+
+  private jsonPlugin         = require('json-reader-writer');
 
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
@@ -19,14 +22,14 @@ export class AppService {
     this.config = this.configService.get<string>('services.amrod');  
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_11PM)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async updateProductsPricesBrands(){
 
     try {
       this.logger.log(`Synchronizing amrod categories, products, prices and brands.`);
 
-      let { credentials } = this.config;
-
+      let { credentials } = this.config, fs = require('fs');
+      
       this.amrodService
           .login(credentials)
           .then( async() => {
@@ -43,13 +46,25 @@ export class AppService {
             // Fetch amrod products
             let products = await this.amrodService.getProducts();    
 
-            await this.cacheManager.set('amrod_categories', categories, 0);
-            
-            await this.cacheManager.set('amrod_products', products, 0);
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}brands.json`,
+              JSON.stringify(brands)
+            );
 
-            await this.cacheManager.set('amrod_prices', prices, 0);
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}categories.json`,
+              JSON.stringify(categories)
+            );
             
-            await this.cacheManager.set('amrod_brands', brands, 0);
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}prices.json`,
+              JSON.stringify(prices)
+            );
+
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}products.json`,
+              JSON.stringify(products)
+            );
 
             this.logger.log(`Done synchronizing amrod categories, products, prices and brands.`);
 
@@ -70,7 +85,7 @@ export class AppService {
 
       this.logger.log(`Synchronizing amrod data sets.`);
 
-      let { credentials } = this.config;
+      let { credentials } = this.config,fs = require('fs');
 
       this.amrodService
           .login(credentials)
@@ -81,10 +96,16 @@ export class AppService {
 
             // Fetch amrod brands
             let brands = await this.amrodService.getBrands();  
-          
-            await this.cacheManager.set('amrod_stock',stock, 0);
 
-            await this.cacheManager.set('amrod_brands',brands, 0);
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}brands.json`,
+              JSON.stringify(brands)
+            );
+
+            await fs.writeFileSync(
+              `${process.cwd()}${sep}public${sep}amrod${sep}stock.json`,
+              JSON.stringify(stock)
+            );
 
             this.logger.log(`Done synchronizing amrod data sets.`);
 
