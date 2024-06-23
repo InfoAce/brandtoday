f67ad623d2a94d0860d816f16a441dc5fbb588d9
Bryant Rotich <bryantkrotich@gmail.com>
2024-06-23 23:57:11 +0300

    Update: api and pesapal configurations

--- backend/src/controllers/dashboard/system/system.controller.ts
+++ backend/src/controllers/dashboard/system/system.controller.ts
@@ -35,12 +35,25 @@ export class SystemController {
 
     @UseGuards(AdminGuard)
     @Post('configurations')
+    /**
+     * Update configurations
+     * 
+     * @param {Object} body - The body of the request containing the configurations
+     * @param {Object} req - The request object
+     * @param {Object} res - The response object
+     * @returns {Promise<Object>} - A promise that resolves to the updated configurations
+     */
     updateConfigurations(@Body() body: any, @Req() req: Request,  @Res() res: Response) {
         try{
-            let { credentials } = this.configService.get<any>('services.amrod'), fs = require('fs');
+            // Get amrod credentials from the configuration service
+            let { credentials } = this.configService.get<any>('services.amrod'),
+                // Import the file system module
+                fs = require('fs');
             
+            // Write the configurations to the file
             fs.writeFileSync(this.file_path,JSON.stringify(body));
 
+            // Login to amrod
             this.amrodService
                 .login(credentials)
                 .then( async() => {
@@ -57,24 +70,31 @@ export class SystemController {
                     // Fetch amrod products
                     let products = await this.amrodService.getProducts();    
 
-                    // Fetch amrod prices
+                    // Fetch amrod stock
                     let stock = await this.amrodService.getStock();  
                     
+                    // Write the amrod brands to a json file
                     await fs.writeFileSync(`${process.cwd()}${sep}public${sep}amrod${sep}brands.json`,JSON.stringify(brands));
                     
+                    // Write the amrod categories to a json file
                     await fs.writeFileSync(`${process.cwd()}${sep}public${sep}amrod${sep}categories.json`,JSON.stringify(categories));
                     
+                    // Write the amrod prices to a json file
                     await fs.writeFileSync(`${process.cwd()}${sep}public${sep}amrod${sep}prices.json`,JSON.stringify(prices));
         
+                    // Write the amrod products to a json file
                     await fs.writeFileSync(`${process.cwd()}${sep}public${sep}amrod${sep}products.json`,JSON.stringify(products));
 
+                    // Write the amrod stock to a json file
                     await fs.writeFileSync(`${process.cwd()}${sep}public${sep}amrod${sep}stock.json`,JSON.stringify(stock));
 
                 });
 
+            // Return the updated configurations
             return res.status(HttpStatus.OK).json({ configurations: this.jsonPlugin.readJSON(this.file_path) });
-            
+
         } catch(err) {
+            // Return an error response if an error occurred
             return res.status(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
