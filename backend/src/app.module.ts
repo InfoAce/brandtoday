5e36fc33d537344f4a0504545263cd066f1df812
Bryant Rotich <bryantkrotich@gmail.com>
2024-07-09 21:19:00 +0300

    Fix: typeorm sychronize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -74,7 +74,7 @@ import { TimelineEntity } from './entities/timeline.entity';
           TransactionEntity,
           UserEntity
         ],
-        synchronize: true,
+        synchronize: false,
         subscribers: [
           OrderSubscriber,
           UserSubscriber
