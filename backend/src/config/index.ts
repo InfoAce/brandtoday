e8f7f0a3e4a913535c1348fd18c1fe99b0ba6986
Bryant Rotich <bryantkrotich@gmail.com>
2024-08-06 13:19:23 +0300

    Update: add product colors

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,3 +1,4 @@
 export { default as ConfigApp } from './app';
+export { default as ConfigColors } from './colors';
 export { default as ConfigServices } from './services';
 export { default as ConfigDatabase } from './database';
