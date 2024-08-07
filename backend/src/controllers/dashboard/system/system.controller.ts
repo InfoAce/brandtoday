300ed54c9bf8d6dfe3a81e88ba7cabd0483858e1
Bryant Rotich <bryantkrotich@gmail.com>
2024-08-07 11:00:19 +0300

    Fix: sync amrod data to db

--- backend/src/controllers/dashboard/system/system.controller.ts
+++ backend/src/controllers/dashboard/system/system.controller.ts
@@ -11,6 +11,7 @@ import { sep } from 'path';
 import StockModel from 'src/models/stock.model';
 import { ILike } from 'typeorm';
 import { delay } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('dashboard/system')
 export class SystemController {
@@ -70,39 +71,72 @@ export class SystemController {
             await this.amrodService.login(credentials);
 
             // Fetch amrod product categories
-            let categories = await this.amrodService.getCategories();  
+            let categories = (await this.amrodService.getCategories()).map( category => ({ ...category, id: uuidv4() }));  
 
             // Fetch amrod prices
-            let prices = await this.amrodService.getPrices();   
+            // let prices = await this.amrodService.getPrices();   
 
             // Fetch amrod brands
             let brands = await this.amrodService.getBrands();  
             
-            // // Fetch amrod products
-            let products = await this.amrodService.getProducts();    
+            // // // Fetch amrod products
+            let products = (await this.amrodService.getProducts()).map( category => ({ ...category, id: uuidv4() }));    
 
-            // Fetch amrod stock
-            let stocks = await this.amrodService.getStock();
-                       
-           await this.brandModel.saveMany(brands.map( ({ code, image, name}) => ({ code, name, image })));
+            // // Fetch amrod stock
+            // let stocks = await this.amrodService.getStock();
 
-        //    categories = (await this.categoryModel.saveMany( categories.map( ({ categoryName: name, categoryCode: code, categoryPath: path }) => ({ code, name, path }) ) )).genratedMaps.map( (category,index) => ({ category, ...categories[index] );
+            let sub_categories      = categories.map( category => category.children.map( sub_category => ({ ...sub_category, id: uuidv4(), category_id: category.id, children: sub_category.children }) ) ).flat();
 
-            await Promise.all([
-                categories.map( 
-                    async ({ children, categoryName: name, categoryCode: code, categoryPath: path }) => {                
-                        let { generatedMaps: [ category ] } = await this.categoryModel.insert({ code, name, path});
-                        return await Promise.all([
-                            children.map( async ({ categoryName, categoryCode, categoryPath, children: sub_children }) => {
-                                let { generatedMaps: [ sub_category ] } = await this.subCategoryModel.insert({ code: categoryCode, name: categoryName, path: categoryPath, category_id: category.id });
-                                return await this.childSubCategory.insert(
-                                    sub_children.map( async ({ categoryName, categoryCode, categoryPath }) => ({ code: categoryCode, name: categoryName, path: categoryPath, sub_category_id: sub_category.id }))
-                                );
-                            })
-                        ])
+            let child_sub_categories = sub_categories.map( sub_category => sub_category.children.map( child_sub_category => ({ ...child_sub_category, id: uuidv4(), sub_category_id: sub_category.id }) ) ).flat();
+
+            let product_categories  = products.map( 
+                product => product.categories.map( 
+                    category => {
+                        let path = category.path.split('/');
+                        if( path.length > 2) {
+                            let fetched_sub_category       = sub_categories.find( fetched_sub_category => fetched_sub_category.categoryPath == take(path,2).join('/') );
+                            let fetched_child_sub_category = child_sub_categories.find( fetched_child_sub_category => fetched_child_sub_category.categoryPath == take(path,3).join('/') );
+                            return { 
+                                id: uuidv4(), 
+                                category_id:           fetched_sub_category.category_id, 
+                                sub_category_id:       fetched_child_sub_category.sub_category_id, 
+                                child_sub_category_id: fetched_child_sub_category.id, 
+                                product_id:   product.id 
+                            }
+                        }
+                        if( path.length == 2) {
+                            let fetched_sub_category = sub_categories.find( fetched_sub_category => fetched_sub_category.categoryPath == take(path,2).join('/') );
+                            return {  
+                                id: uuidv4(), 
+                                category_id: fetched_sub_category.category_id, 
+                                sub_category_id: fetched_sub_category.id, 
+                                product_id:   product.id 
+                            }
+                        }
+                        if( path.length == 1) {
+                            let fetched_category = categories.find( fetched_category => fetched_category.categoryPath == take(path,1).join('/') );
+                            return { id: uuidv4(), category_id: fetched_category.id, product_id: product.id }
+                        }
                     }
+                ) 
+            ).flat();
+
+            await this.categoryModel.insert(categories.map( ({categoryName: name, categoryCode: code, categoryPath: path, id }) => ({ id, code, name, path}) ))
+
+            await this.subCategoryModel.insert(sub_categories.map( ({categoryName: name, categoryCode: code, categoryPath: path, id, category_id }) => ({ id, category_id, code, name, path}) ))
+
+            await this.childSubCategory.insert(child_sub_categories.map( ({categoryName: name, categoryCode: code, categoryPath: path, id, sub_category_id }) => ({ id, sub_category_id, code, name, path}) ))
+                       
+            await this.brandModel.saveMany(brands.map( ({ code, image, name}) => ({ code, name, image })));
+
+            await this.productModel.insert(
+                products.map( 
+                    ({ id, fullCode: full_code, simpleCode: simple_code, price: amount, gender, images, variants, brandingTemplates: branding_templates, colourImages: colour_images, fullBrandingGuide: full_branding_guide, logo24BrandingGuide: logo_branding_guide, description, productName: name, companionCodes: companion_codes, categories }) => 
+                        ({ id, full_code, simple_code, amount, gender, branding_templates, variants, images, colour_images, companion_codes, description, full_branding_guide, logo_branding_guide, name }) 
                 )
-            ]);
+            );
+
+            await this.productCategoryModel.saveMany(product_categories.map( (product_category) => product_category ));
 
             // await Promise.all([
             //     products.map( 
@@ -152,7 +186,7 @@ export class SystemController {
             // ]);    
 
             // Return the updated configurations
-            return res.status(HttpStatus.OK).json({ configurations: this.jsonPlugin.readJSON(this.file_path) });
+            return res.status(HttpStatus.OK).json({ configurations: this.jsonPlugin.readJSON(this.file_path), product_categories });
 
         } catch(error) {
             this.logger.error(error);
--- backend/src/models/product.model.ts
+++ backend/src/models/product.model.ts
@@ -12,110 +12,129 @@ import { ModelException } from 'src/exceptions/model.exception';
 
 @Injectable()
 export default class ProductModel {
+/**
+ * BrandModel constructor.
+ * 
+ * @param productRepository - The product repository.
+ */
+constructor(
   /**
-   * BrandModel constructor.
-   * 
-   * @param productRepository - The product repository.
+   * The product repository.
+   * This repository is injected by NestJS using the @InjectRepository decorator.
+   * It is used to perform database operations related to the ProductEntity.
    */
-  constructor(
-    /**
-     * The product repository.
-     * This repository is injected by NestJS using the @InjectRepository decorator.
-     * It is used to perform database operations related to the ProductEntity.
-     */
-    @InjectRepository(ProductEntity)
-    private productRepository: ProductRepository,
-  ) {}
-  
-  /**
-   * Finds multiple product entities based on the provided data.
-   *
-   * @param {any} data - The data used to find the products.
-   * @return {Promise<ProductEntity[]>} A promise that resolves to an array of found products.
-   * @throws {ModelException} Throws a ModelException if there is an error finding the products.
-   */
-  async find(data: any): Promise<ProductEntity[]> {
-    try {
-      // Find multiple product entities based on the provided data.
-      // The find method of the productRepository is used to find the products.
-      return await this.productRepository.find(data);
-    } catch (error) {
-      // If there is an error finding the products, throw a ModelException with the error message.
-      throw new ModelException(error);
-    }
-  }
+  @InjectRepository(ProductEntity)
+  private productRepository: ProductRepository,
+) {}
 
-  /**
-   * Finds a single product entity based on the provided data.
-   * Throws a ModelException if the product is not found.
-   *
-   * @param {any} data - The data used to find the product.
-   * @return {Promise<ProductEntity>} A promise that resolves to the found product.
-   * @throws {ModelException} Throws a ModelException if the product is not found.
-   */
-  async findOne(data: any): Promise<ProductEntity> {
-    try {
-      // Finds a single product entity based on the provided data and throws an exception if it is not found.
-      // The findOneOrFail method of the productRepository is used to find the product entity.
-      return await this.productRepository.findOneOrFail(data);
-    } catch (error) {
-      // If the product is not found, throw a ModelException with the error message.
-      throw new ModelException(error);
-    }
+/**
+ * Finds multiple product entities based on the provided data.
+ *
+ * @param {any} data - The data used to find the products.
+ * @return {Promise<ProductEntity[]>} A promise that resolves to an array of found products.
+ * @throws {ModelException} Throws a ModelException if there is an error finding the products.
+ */
+async find(data: any): Promise<ProductEntity[]> {
+  try {
+    // Find multiple product entities based on the provided data.
+    // The find method of the productRepository is used to find the products.
+    return await this.productRepository.find(data);
+  } catch (error) {
+    // If there is an error finding the products, throw a ModelException with the error message.
+    throw new ModelException(error);
   }
+}
 
-  /**
-   * Paginates the product entities based on the provided pagination options.
-   *
-   * @param {IPaginationOptions} options - The pagination options.
-   * @return {Promise<Pagination<ProductEntity>>} A promise that resolves to the paginated product entities.
-   */
-  async paginate(
-    options: IPaginationOptions,
-  ): Promise<Pagination<ProductEntity>> {
-    // Paginates the product entities using the paginate function from the 'nestjs-typeorm-paginate' library.
-    // The productRepository is passed as the first argument, and the options are passed as the second argument.
-    return paginate<ProductEntity>(this.productRepository, options);
+/**
+ * Finds a single product entity based on the provided data.
+ * Throws a ModelException if the product is not found.
+ *
+ * @param {any} data - The data used to find the product.
+ * @return {Promise<ProductEntity>} A promise that resolves to the found product.
+ * @throws {ModelException} Throws a ModelException if the product is not found.
+ */
+async findOne(data: any): Promise<ProductEntity> {
+  try {
+    // Finds a single product entity based on the provided data and throws an exception if it is not found.
+    // The findOneOrFail method of the productRepository is used to find the product entity.
+    return await this.productRepository.findOneOrFail(data);
+  } catch (error) {
+    // If the product is not found, throw a ModelException with the error message.
+    throw new ModelException(error);
   }
+}
 
-  /**
-   * Updates a single product entity based on the provided id and data.
-   * Throws a ModelException if the product is not found.
-   *
-   * @param {string} id - The id of the product entity to update.
-   * @param {any} data - The data used to update the product entity.
-   * @return {Promise<any>} A promise that resolves to the updated product entity.
-   * @throws {ModelException} Throws a ModelException if the product entity is not found.
-   */
-  async updateOne(id: string, data: any): Promise<any> {
-    try {
-      // Updates a single product entity based on the provided id and data.
-      // The productRepository's update method is used to update the product entity.
-      return await this.productRepository.update({id}, data);
-    } catch (error) {
-      // If the product entity is not found, throw a ModelException with the error message.
-      throw new ModelException(error);
-    } 
-  }
+/**
+ * Paginates the product entities based on the provided pagination options.
+ *
+ * @param {IPaginationOptions} options - The pagination options.
+ * @return {Promise<Pagination<ProductEntity>>} A promise that resolves to the paginated product entities.
+ */
+async paginate(
+  options: IPaginationOptions,
+): Promise<Pagination<ProductEntity>> {
+  // Paginates the product entities using the paginate function from the 'nestjs-typeorm-paginate' library.
+  // The productRepository is passed as the first argument, and the options are passed as the second argument.
+  return paginate<ProductEntity>(this.productRepository, options);
+}
 
-  /**
-   * Saves a single product entity based on the provided data.
-   * Throws a ModelException if there is an error saving the product.
-   *
-   * @param {any} data - The data used to save the product.
-   * @return {Promise<any>} A promise that resolves to the saved product.
-   * @throws {ModelException} Throws a ModelException if there is an error saving the product.
-   */
-  async save(data: any): Promise<any> {
-    try {
-      // Saves a single product entity based on the provided data.
-      // The productRepository's save method is used to save the product entity.
-      return await this.productRepository.save(data);
-    } catch (error) {
-      // If there is an error saving the product, throw a ModelException with the error message.
-      throw new ModelException(error);
-    }
+/**
+ * Updates a single product entity based on the provided id and data.
+ * Throws a ModelException if the product is not found.
+ *
+ * @param {string} id - The id of the product entity to update.
+ * @param {any} data - The data used to update the product entity.
+ * @return {Promise<any>} A promise that resolves to the updated product entity.
+ * @throws {ModelException} Throws a ModelException if the product entity is not found.
+ */
+async updateOne(id: string, data: any): Promise<any> {
+  try {
+    // Updates a single product entity based on the provided id and data.
+    // The productRepository's update method is used to update the product entity.
+    return await this.productRepository.update({id}, data);
+  } catch (error) {
+    // If the product entity is not found, throw a ModelException with the error message.
+    throw new ModelException(error);
+  } 
+}
+
+/**
+ * Saves a single product entity based on the provided data.
+ * Throws a ModelException if there is an error saving the product.
+ *
+ * @param {any} data - The data used to save the product.
+ * @return {Promise<any>} A promise that resolves to the saved product.
+ * @throws {ModelException} Throws a ModelException if there is an error saving the product.
+ */
+async save(data: any): Promise<any> {
+  try {
+    // Saves a single product entity based on the provided data.
+    // The productRepository's save method is used to save the product entity.
+    return await this.productRepository.save(data);
+  } catch (error) {
+    // If there is an error saving the product, throw a ModelException with the error message.
+    throw new ModelException(error);
   }
+}
+
+/**
+ * Saves a single product entity based on the provided data.
+ * Throws a ModelException if there is an error saving the product.
+ *
+ * @param {any} data - The data used to save the product.
+ * @return {Promise<any>} A promise that resolves to the saved product.
+ * @throws {ModelException} Throws a ModelException if there is an error saving the product.
+ */
+async insert(data: any): Promise<any> {
+  try {
+    // Save a single product entity by calling the insert method of the productRepository.
+    // The insert method is used to insert a new product entity into the database.
+    return await this.productRepository.insert(data);
+  } catch (error) {
+    // If there is an error saving the product, throw a ModelException with the error message.
+    throw new ModelException(error);
+  }
+}
 
 /**
  * Saves multiple product entities based on the provided data.
