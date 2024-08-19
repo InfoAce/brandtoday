3aeaa725cfac735bde2fffb0c93946655cc82e03
Bryant Rotich <bryantkrotich@gmail.com>
2024-08-19 12:19:02 +0300

    Fix: chunking

--- backend/src/controllers/dashboard/system/system.controller.ts
+++ backend/src/controllers/dashboard/system/system.controller.ts
@@ -150,7 +150,7 @@ export class SystemController {
             await this.productCategoryModel.insert( product_categories.map( (product_category) => product_category ) );
             
             await Promise.all(
-                chunk(variants,1000).map( async (variants) => {
+                chunk(variants,500).map( async (variants) => {
                     return new Promise( (resolve,reject) => {
                         setTimeout( async () => {
                             await this.productVariantModel.insert( 
