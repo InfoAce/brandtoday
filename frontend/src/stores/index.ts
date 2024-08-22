0713321f63610f73c82b97449c50cf0a474bb73e
Bryant Rotich <bryantkrotich@gmail.com>
2024-08-22 23:24:38 +0300

    Fix: assets url

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -36,7 +36,7 @@ export default createStore({
   },
   getters:{
     auth:              (state) => state.auth,
-    assetsUrl:         (state) => state.env.VITE_API_URL.replace('api/v1',''),
+    assetsUrl:         (state) => state.env.VITE_API_URL.replace('/api/v1',''),
     banner_height:      (state) => state.device_width * 0.3,
     cart:              (state) => state.cart,
     device_width:      (state) => state.device_width,
