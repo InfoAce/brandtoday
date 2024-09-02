4e16e01d0084f19202b5e21bf77c53ffe2bcffae
Bryant Rotich <bryantkrotich@gmail.com>
2024-09-02 20:11:00 +0300

    Update: build setup configuration

--- frontend/src/dashboard.ts
+++ frontend/src/dashboard.ts
@@ -5,7 +5,7 @@ import api from './api'
 
 import { createApp } from 'vue'
 
-import App from '..vue'
+import App from './App.vue'
 import router from './router'
 
 import VueSweetalert2 from 'vue-sweetalert2';
@@ -13,16 +13,14 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 import toast from './toast';
 
-window.document.querySelector('title').innerHTML = `${import.meta.env.VITE_APP_NAME}`;
+// window.document.querySelector('title').innerHTML = `${import.meta.env.VITE_APP_NAME}`;
 
-import { VueReCaptcha } from 'vue-recaptcha-v3';
 import storage from './storage';
 
 import "skeleton-screen-css";
-import { RouterView } from 'vue-router';
 // import themes from './theme';
 
-const app = createApp(RouterView)
+const app = createApp(App)
 
 app.use(toast);
 app.use(appStore)
@@ -30,6 +28,5 @@ app.use(storage);
 app.use(router)
 app.use(api);
 app.use(VueSweetalert2);
-app.use(VueReCaptcha, { siteKey: '6LfUKLkpAAAAAMFo0iomiilG1QUpAkw8Xph5tz-Y' })
 // app.use(themes,{ router });
 app.mount('#app')
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "url";
 import { resolve } from 'path';
-import Pages from 'vite-plugin-pages'
+import multipleHtml from '@beta-lib/vite-multiple-html';
 
 export default defineConfig( ({command,mode}) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -23,6 +23,52 @@ export default defineConfig( ({command,mode}) => {
     },
     plugins: [
       vue(),
+      multipleHtml({
+        /**
+         * you can add more item in packageList to set multiple pages
+         */
+        packageList: [
+          {
+              /**
+               * you will not need to add script tags in `index.html`, the original tags need to be deleted
+               */
+              entry: 'src/main.ts',
+              /**
+               * filename is your html file's name. When you run your project, this will be your Access Name, and it will also be used as the file name of the packaged output HTML
+               */
+              filename: 'index',
+              /**
+               * templatePath is the path to the template html
+               */
+              templatePath: 'index.html'
+          },
+          {
+            /**
+             * you will not need to add script tags in `index.html`, the original tags need to be deleted
+             */
+            entry: 'src/main.ts',
+            /**
+             * filename is your html file's name. When you run your project, this will be your Access Name, and it will also be used as the file name of the packaged output HTML
+             */
+            filename: 'dashboard',
+            /**
+             * templatePath is the path to the template html
+             */
+            templatePath: 'dashboard.html'
+          }
+        ],
+        /**
+         * injectData will be inject to html, Note: multiple pages share the same injectData
+         */
+        injectData:{
+          title: 'Testing'
+        },
+        /**
+         * tempHtmlDir is used to store HTML files generated from template HTML
+         * @default htmlTemp
+         */
+        tempHtmlDir: 'dist'
+      })
     ],
     resolve: {
       alias: {
